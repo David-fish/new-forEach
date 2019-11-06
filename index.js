95f0fd5225ee5f2428fd28744801e85a40a1a184
David-fish <2472774684@qq.com>
2019-11-06 09:05:09 +0800

    111

---
+++ index.js
@@ -0,0 +1,8 @@
+let arr=[1,2,3,4,5,6,'q','w','e','r']
+    function newfor(arr){
+        for(var i=0;i<arr.length;i++){
+            console.log(arr[i]);
+        }
+    }
+    // newfor(arr)
+    console.log(newfor(arr));
